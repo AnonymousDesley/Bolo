f8228bd405f9267722155380a7a081c507b983ba
Boyema <desleyboyema@gmail.com>
2025-10-19 02:28:15 +0100

    Revert "feat: implement new landing page design based on user description"
    This reverts commit e916d04d2be9657df0e57576891072f0cc6f6586.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 "use client";
 
 import ChatUI from "@/components/ChatUI";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="main-container bg-[#181516] text-white min-h-screen">
+      <Header />
       <ChatUI />
     </main>
   );
